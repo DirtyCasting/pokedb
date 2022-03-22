d6c6ffdac86ae8f97fe3a6b15acd76a818f42a3c
DirtyCasting <dminahan98@gmail.com>
2022-03-22 15:21:03 -0400

    cleaned up code, added fetching of individual pages

--- scraper.py
+++ scraper.py
@@ -3,21 +3,43 @@ import requests
 import re
 from bs4 import BeautifulSoup
 from pprint import pprint
+import os
+import time
 
-national_dex_url = "https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
-national_dex_page = requests.get(national_dex_url)
-data = national_dex_page.text
+base_url = "https://bulbapedia.bulbagarden.net"
+master_list = []
 
-html = BeautifulSoup(data, 'html.parser')
-tables = html.findAll("table")
-pokemon_links = []
+def get_master_list():
+    national_dex_url = "/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
+    national_dex_page = requests.get(base_url + national_dex_url)
+    data = national_dex_page.text
 
-# nested for loop intended to find all the links in each table, then append them to the
-# master list above (pokemon_links) after checking for duplicates
-for table in tables:
-    for link in table.findAll("a", attrs={"href": re.compile("/wiki/.+\(Pok%C3%A9mon\)")}):
-        potential = link.get("href")
-        if potential not in pokemon_links:
-            pokemon_links.append(potential)
+    html = BeautifulSoup(data, 'html.parser')
+    tables = html.findAll("table")
+    pokemon_links = []
 
-pprint(pokemon_links)
+    # nested for loop intended to find all the links in each table, then append them to the
+    # master list above (pokemon_links) after checking for duplicates
+    for table in tables:
+        for link in table.findAll("a", attrs={"href": re.compile("/wiki/.+\(Pok%C3%A9mon\)")}):
+            potential = link.get("href")
+            if potential not in pokemon_links:
+                pokemon_links.append(potential)
+    
+    return pokemon_links
+
+
+
+master_list += (get_master_list())
+os.mkdir('./wiki')
+
+for pokemon_page in master_list:
+    print(f"Fetching '{pokemon_page}'...")
+    page = requests.get(base_url + pokemon_page)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    pokemon_name = soup.title.text.split()[0]
+
+    with open(f'wiki/{pokemon_name}.html', 'w+') as pokemon_file:
+        pokemon_file.write(page.text)
+    time.sleep(1)    
+
